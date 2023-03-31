bc50b9e4049e13a56149ffb801a6b37957e3c02d
azamatmurzaliev <Lirog.kg@gmail.com>
2023-03-31 17:13:37 +0600

    добавил фикстуры

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -2,9 +2,7 @@ import crypto from "crypto";
 import mongoose from "mongoose";
 import config from "./config";
 import User from "./models/User";
-import Artist from "./models/Artist";
-import Album from "./models/Album";
-import Track from "./models/Track";
+import Cocktail from "./models/Cocktail";
 
 const run = async () => {
   mongoose.set("strictQuery", false);
@@ -12,188 +10,18 @@ const run = async () => {
   const db = mongoose.connection;
 
   try {
-    await db.dropCollection("albums");
     await db.dropCollection("cocktails");
-    await db.dropCollection("trackhistories");
-    await db.dropCollection("tracks");
     await db.dropCollection("users");
   } catch (e) {
     console.log("Collections were not present, skipping drop...");
   }
 
-  const [eminem, madonna] = await Artist.create(
-    {
-      name: "Eminem",
-      image: "fixtures/eminem.jpg",
-      info: "Rap singer",
-    },
-    {
-      name: "Madonna",
-      image: "fixtures/madonna.jpg",
-      info: "Pop singer",
-    }
-  );
-
-  const [theEminemShow, encore, americanLife, trueBlue] = await Album.create(
-    {
-      name: "Encore",
-      year: 2004,
-      artist: eminem._id,
-      image: "fixtures/encore.jpg",
-    },
-    {
-      name: "The Eminem Show",
-      year: 2002,
-      artist: eminem._id,
-      image: "fixtures/the_eminem_show.jpg",
-    },
-    {
-      name: "American Life",
-      year: 2003,
-      artist: madonna._id,
-      image: "fixtures/american_life.jpg",
-    },
-    {
-      name: "True Blue",
-      year: 1986,
-      artist: madonna._id,
-      image: "fixtures/true_blue.jpg",
-    }
-  );
-
-  await Track.create(
-    {
-      name: "White America",
-      time: "3:14",
-      album: theEminemShow._id,
-      trackNumber: 1,
-      linkToYoutube: "bi-8QeEm0HM",
-    },
-    {
-      name: "Business",
-      time: "3:15",
-      album: theEminemShow._id,
-      trackNumber: 3,
-    },
-    {
-      name: "Soldier",
-      time: "2:18",
-      album: theEminemShow._id,
-      trackNumber: 2,
-      linkToYoutube: "HOCdIG3mq1U",
-    },
-    {
-      name: "Say Goodbye Hollywood",
-      time: "3:22",
-      album: theEminemShow._id,
-      trackNumber: 4,
-    },
-    {
-      name: "Without Me",
-      time: "3:25",
-      album: theEminemShow._id,
-      trackNumber: 5,
-    },
-    {
-      name: "Evil Deeds",
-      time: "3:16",
-      album: encore._id,
-      trackNumber: 1,
-    },
-    {
-      name: "Mosh",
-      time: "3:15",
-      album: encore._id,
-      trackNumber: 4,
-    },
-    {
-      name: "Rain Man",
-      time: "3:11",
-      album: encore._id,
-      trackNumber: 3,
-    },
-    {
-      name: "Just Lose It",
-      time: "3:18",
-      album: encore._id,
-      trackNumber: 2,
-    },
-    {
-      name: "Mockingbird",
-      time: "3:40",
-      album: encore._id,
-      trackNumber: 5,
-    },
-    {
-      name: "Frozen",
-      time: "3:33",
-      album: americanLife._id,
-      trackNumber: 5,
-      linkToYoutube: "XS088Opj9o0",
-    },
-    {
-      name: "Hollywood",
-      time: "3:35",
-      album: americanLife._id,
-      trackNumber: 4,
-    },
-    {
-      name: "Love Profusion",
-      time: "3:39",
-      album: americanLife._id,
-      trackNumber: 3,
-    },
-    {
-      name: "Nothing Fails",
-      time: "3:05",
-      album: americanLife._id,
-      trackNumber: 2,
-    },
-    {
-      name: "Intervention",
-      time: "3:07",
-      album: americanLife._id,
-      trackNumber: 1,
-    },
-    {
-      name: "Open Your Heart",
-      time: "3:11",
-      album: trueBlue._id,
-      trackNumber: 4,
-    },
-    {
-      name: "White Heat",
-      time: "3:15",
-      album: trueBlue._id,
-      trackNumber: 3,
-    },
-    {
-      name: "Live to Tell",
-      time: "3:19",
-      album: trueBlue._id,
-      trackNumber: 5,
-    },
-    {
-      name: "Where’s the Party",
-      time: "3:58",
-      album: trueBlue._id,
-      trackNumber: 2,
-    },
-    {
-      name: "La Isla Bonita",
-      time: "3:47",
-      album: trueBlue._id,
-      trackNumber: 1,
-    }
-  );
-
-  await User.create(
+  const [azamat, adilet] = await User.create(
     {
       username: "Azamat",
       password: "12345",
       displayName: "Aza",
       token: crypto.randomUUID(),
-
     },
     {
       username: "Adilet",
@@ -201,6 +29,41 @@ const run = async () => {
       displayName: "Adik",
       token: crypto.randomUUID(),
       role: "admin",
+    }
+  );
+
+  await Cocktail.create(
+    {
+      user: adilet._id,
+      name: "Long Island",
+      image: "fixtures/long.jpeg",
+      recipe: "about recipe",
+      ingredients: [
+        {
+          name: "white rum",
+          amount: "10ml",
+        },
+        {
+          name: "tequila",
+          amount: "50ml",
+        },
+      ],
+    },
+    {
+      user: azamat._id,
+      name: "Mary",
+      image: "fixtures/mary.jpeg",
+      recipe: "about recipe",
+      ingredients: [
+        {
+          name: "water",
+          amount: "1 bottle",
+        },
+        {
+          name: "yellow lemon",
+          amount: "2 slice",
+        },
+      ],
     }
   );
 
